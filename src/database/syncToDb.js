f0ade2c72bee50300cd5d1dce55731ffbc1f946f
Kieran White <124150925+Kieran-White@users.noreply.github.com>
2024-05-24 11:25:50 +0100

    Linked database to python

--- src/database/syncToDb.js
+++ src/database/syncToDb.js
@@ -1,10 +1,20 @@
 import updateDocument from "./dbUpdate";
+import SetLED from "../python/led_controller.py";
 
 const syncToDb = async (id, dataKeyPair, widgets) => {
     const newRoom = widgets[id];
     const response = await updateDocument(newRoom, dataKeyPair);
 
-    console.log(response);
+    if (newRoom === "Living Room") {
+        if ("isLightOn" in dataKeyPair) {
+            if (!dataKeyPair[1]) {
+                SetLED("#000000")
+            }
+        }
+        if ("colour" in dataKeyPair) {
+            SetLED(dataKeyPair[1])
+        }
+    }
 }
 
 export default syncToDb;
