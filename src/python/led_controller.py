908a85c48cc1b28fa511f3e51c5911f72c108c0e
Kieran White <124150925+Kieran-White@users.noreply.github.com>
2024-05-24 10:14:10 +0100

    Set up LED gpio pins

--- src/python/led_controller.py
+++ src/python/led_controller.py
@@ -2,15 +2,16 @@
 
 pi = pigpio.pi()
 
-RED_PIN = "" #GPIO Pin Number
-GREEN_PIN = "" #GPIO Pin Number
-BLUE_PIN = "" #GPIO Pin Number
+RED_PIN = 25 #GPIO Pin Number
+GREEN_PIN = 24 #GPIO Pin Number
+BLUE_PIN = 23 #GPIO Pin Number
 
 pi.set_mode(RED_PIN, pigpio.OUTPUT)
 pi.set_mode(GREEN_PIN, pigpio.OUTPUT)
 pi.set_mode(BLUE_PIN, pigpio.OUTPUT)
 
-def SetLED(r, g, b):
+def SetLED(hexcode):
+    r, g, b = hex_to_rgb(hexcode)
     pi.set_PWM_dutycycle(RED_PIN, r)
     pi.set_PWM_dutycycle(GREEN_PIN, g)
     pi.set_PWM_dutycycle(BLUE_PIN, b)
@@ -18,4 +19,10 @@ def SetLED(r, g, b):
 def cleanup():
     pi.stop()
 
-    
+def hex_to_rgb(hexcode): #Gotta flip cause common anode LED
+    hexcode = hexcode.lstrip('#')
+    red = 255 - int(hexcode[0:2], 16)
+    green = 255 - int(hexcode[2:4], 16)
+    blue = 255 - int(hexcode[4:6], 16)
+
+    return red, green, blue
